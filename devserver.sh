34755a33a72bf3b7f528e0050a43c8b4080a5f0f
Vinicius <sjf.vinicius@gmail.com>
2026-04-08 12:46:38 -0300

    bugfix: last version of devserver.sh

--- devserver.sh
+++ devserver.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # python3 -m venv .venv
+# rm -rf .venv310
 # python3.10 -m venv .venv310
 # source .venv/bin/activate
 # source .venv310/bin/activate
 # source ./venv/Scripts/activate
 # source ./venv310/Scripts/activate
 
+pip install --upgrade pip
+pip install -r requirements.txt
 # Necessário instalar o FFMPEG para habilitar o funcionamento de conexão em câmeras RTSP.
 
 # python3 -u -m flask --app main run --debug
